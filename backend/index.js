55bce1ca5ff7ded161003224c98ad130363145d1
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-04-03 03:14:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
 
-const PORT = 4000; // backend routing port
+const PORT = process.env.port || 4000; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
