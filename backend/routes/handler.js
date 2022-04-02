1f76a07e0a600c383679f6848782096ba0703108
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-04-03 03:32:21 +0530

    Update handler.js

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req,res) => {
+    res.send("checking");
+}
+
 router.get('/tweets', (req, res) => {
     const str = [
         {
